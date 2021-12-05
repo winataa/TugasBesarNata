f9caa825440d1b0d54232bb1203ac7f9a2282e3d
nandaadyatma <putunanda928@gmail.com>
2021-12-05 14:48:32 +0800

    Menambahkan sistem login username dan password, kemudian jika login berhasil sistem akan mengantarkan user ke menu

--- prototype_HelloHealth.c
+++ prototype_HelloHealth.c
@@ -1,4 +1,10 @@
 #include <stdio.h>
+#include <string.h>
+
+#define BOLD "\e[1m"
+#define RED "\e[31m"
+#define GREEN "\e[34m\e[1m"
+#define RESET "\e[0m"
 
 typedef struct kalori_makanan {
 	char nama[50];
@@ -26,18 +32,13 @@ void daftar_makanan() {
 	}	
 	
 void menu(){
+	int masukan;
+	
 	printf ("pilihan menu : \n");
 	printf ("\t 1) Menghitung IMB \n");
 	printf ("\t 2) Daftar kalori makanan \n");
 	printf ("\n");
 	printf ("masukkan pilihan anda ");
-}	
-
-
-	int main() {
-	int masukan;
-	
-	menu();
 	
 	scanf ("%d", &masukan);
 	
@@ -53,8 +54,57 @@ void menu(){
 		default:
 			printf ("Saya kurus");
 	}
+}	
 
+
+	int main() {
+		
+	char inputusername [20];
+	char inputpassword [20];
 	
+	char username[20] = "userapa";
+	char password[20] = "passapa";
+	
+	while (1) {
+			system("cls");
+			system("color F0");
+	
+	printf ("\t\t==LOGIN==\n");
+	printf ("\n");
+	printf ("\tmasukkan username :");
+		scanf ("%s", &inputusername);
+		
+	printf ("\tmasukkan password :");
+		scanf ("%s", &inputpassword);
+	
+		
+	if (strcmp(username, inputusername) == 0 && strcmp(username, inputusername) == 0 )	{
+		system ("cls");
+		
+		printf (GREEN" \n\tusername atau password yang anda masukkan benar\n");
+		
+
+		
+		printf ("selamat datang\n");
+		
+		system ("pause");
+		
+		system ("cls");
+		
+		menu();
+		
+		break;
+		
+	
+	}
+	else {
+		system ("cls");
+		
+		printf (RED BOLD" \n\tusername atau password yang anda masukkan salah\n");
+		
+		system ("pause");
+	}
+	}
 	
 	return 0;
 }
