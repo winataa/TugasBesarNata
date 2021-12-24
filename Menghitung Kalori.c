bbd68e6b3fa8da0ac2073c8215a411c65cee3d72
Putu Nanda Arya Adyatma <93889874+nandaadyatma@users.noreply.github.com>
2021-12-24 13:34:23 +0800

    memperbaiki error dan merapikan

--- Menghitung Kalori.c
+++ Menghitung Kalori.c
@@ -4,6 +4,8 @@
 
 /*Menghitung kalori*/
 int total_kalori = 0;
+int j = 2;
+int i = 2;
 
 typedef struct makanan {
 	char nama_makanan[50];
@@ -13,102 +15,108 @@ typedef struct makanan {
 
 	int i;
 	
+makanan yangDimakan[];
+	
 makanan Makanan[] = {
-	//nama            //gram //kalori
-	{"Nasi Putih      ", 100, 175},
-	{"Nasi Tim        ", 100, 88}, 
-	{"Nasi Uduk       ", 200, 506}, 
-	{"Nasi Goreng     ", 150, 296}, 
-	{"Jagung Rebus    ", 250, 90},
-	{"Singkong Rebus  ", 100, 125},
-	{"Talas Rebus     ", 100, 98},
-	{"Kentang Rebus   ", 200, 166},
-	{"Kentang Goreng  ", 150, 211},
-	{"Ketan Putih     ", 120, 217},
-	{"Ketupat         ", 160, 32},
-	{"Roti Tawar      ", 60, 149},
-	{"Lontong         ", 200, 38},
-	{"Bubur           ", 200, 44},
-	{"Bubur Ayam      ", 200, 165},
-	{"Bubur Sum-sum   ", 100, 178},
-	{"Bihun Goreng    ", 150, 296},
-	{"Crackers        ", 50, 229},
-	{"Makaroni        ", 25, 91},
-	{"Mie Instant     ", 50, 168},
-	{"Mie Goreng      ",100, 263},
-	{"Mie Instant     ", 50, 168},
-	{"Spaghetti       ", 300,642},
+	//nama            //gram //kalori  //indeks
 	
-	{"Ayam Panggang   ", 80, 385},
-	{"Telur Mata Sapi ", 60, 40}
-	{"Telur Ayam Rebus", 60, 97},
-	{"Telur Asin Rebus", 75, 138},
-	{"Ati Ayam Goreng ", 50, 98},
-	{"Bakso Sapi      ", 100, 260},
-	{"Bandeng Goreng  ", 160, 180},
-	{"Lele Goreng     ", 60, 57},
-	{"Tempe Goreng    ", 50, 118},
-	{"Abon Sapi       ", 50, 118},
-	{"Ayam Goreng Kecap", 75, 358},
-	{"Dendeng Balado  ", 50, 147},
-	{"Gulai Ayam      ", 190, 165},
-	{"Gulai Cumi      ", 100, 183},
-	{"Keripik Tempe   ", 25, 68},
-	{"Ayam Kentucky   ", 150, 130},
-	{"Tahu Goreng     ", 150, 130},
-	{"Perkedel Jagung ", 50, 108},
-	{"Perkedel Kentang", 50, 123},
+	//karbohidrat
+	{"Nasi Putih           ", 100, 175},    //0
+	{"Nasi Tim             ", 100, 88},     //1
+	{"Nasi Uduk            ", 200, 506},    //2
+	{"Nasi Goreng          ", 150, 296},    //3
+	{"Jagung Rebus         ", 250, 90},     //
+	{"Singkong Rebus       ", 100, 125},
+	{"Talas Rebus          ", 100, 98},
+	{"Kentang Rebus        ", 200, 166},
+	{"Kentang Goreng       ", 150, 211},
+	{"Ketan Putih          ", 120, 217},
+	{"Ketupat              ", 160, 32},
+	{"Roti Tawar           ", 60, 149},
+	{"Lontong              ", 200, 38},
+	{"Bubur                ", 200, 44},
+	{"Bubur Ayam           ", 200, 165},
+	{"Bubur Sum-sum        ", 100, 178},
+	{"Bihun Goreng         ", 150, 296},
+	{"Crackers             ", 50, 229},
+	{"Makaroni             ", 25, 91},
+	{"Mie Instant          ", 50, 168},
+	{"Mie Goreng           ",100, 263},
+	{"Mie Instant          ", 50, 168},
+	{"Spaghetti            ", 300,642},    //22
+	
+	//Lauk pauk
+	{"Ayam Panggang        ", 80, 385},    //23
+	{"Telur Mata Sapi      ", 60, 40},
+	{"Telur Ayam Rebus     ", 60, 97},
+	{"Telur Asin Rebus     ", 75, 138},
+	{"Ati Ayam Goreng      ", 50, 98},
+	{"Bakso Sapi           ", 100, 260},
+	{"Bandeng Goreng       ", 160, 180},
+	{"Lele Goreng          ", 60, 57},
+	{"Tempe Goreng         ", 50, 118},
+	{"Abon Sapi            ", 50, 118},
+	{"Ayam Goreng Kecap    ", 75, 358},
+	{"Dendeng Balado       ", 50, 147},
+	{"Gulai Ayam           ", 190, 165},
+	{"Gulai Cumi           ", 100, 183},
+	{"Keripik Tempe        ", 25, 68},
+	{"Ayam Kentucky        ", 150, 130},
+	{"Tahu Goreng          ", 150, 130},
+	{"Perkedel Jagung      ", 50, 108},
+	{"Perkedel Kentang     ", 50, 123},
 	{"Fuyung Hai      ", 50, 114},
-	{"Rendang Daging  ", 75, 285},
+	{"Rendang Daging       ", 75, 285},   //43
 	
-	{"Acar Kuning            ", 75, 53},
-	{"Bening Bayam           ", 50, 18},
-	{"Cah Labu Siam          ", 100, 42,
-	{"Sayur Asam             ", 100, 88,
-	{"Sop Ayam               ", 100, 95},
-	{"Sop Bayam              ", 50, 78},
-	{"Sayur Lodeh            ", 100, 59},
-	{"Cah Kacang Panjang     ", 75, 53},
-	{"Tumis Buncis           ", 100, 52},
-	{"Tumis Daun Singkong    ", 75, 53},
-	{"Tumis Kacang + Jagung  ", 75, 53},
-	{"Gudeg                  ", 150, 132},
+	//sayur
+	{"Acar Kuning          ", 75, 53},  //44
+	{"Bening Bayam         ", 50, 18},
+	{"Cah Labu Siam        ", 100, 42},
+	{"Sayur Asam           ", 100, 88},
+	{"Sop Ayam             ", 100, 95},
+	{"Sop Bayam            ", 50, 78},
+	{"Sayur Lodeh          ", 100, 59},
+	{"Cah Kacang Panjang   ", 75, 53},
+	{"Tumis Buncis         ", 100, 52},
+	{"Tumis Daun Singkong  ", 75, 53},
+	{"Tumis Kacang + Jagung", 75, 53},
+	{"Gudeg                ", 150, 132}, //55
 
-	{"Apel               ", 160, 92),
-	{"Apel Merah         ", 140, 82},
-	{"Apel Merah         ", 140, 82},
-	{"Belimbing          ", 160, 80},
-	{"Duku               ", 200, 81},
-	{"Jambu Air          ", 60, 35},
-	{"Jambu Biji         ", 320, 157},
-	{"Jeruk Medan        ", 140, 46},
-	{"Jeruk Pontianak    ", 150, 67},
-	{"Jeruk Sunkist      ", 200, 40},
-	{"Mangga Manalagi    ", 100, 72},
-	{"Nanas              ", 200, 81},
-	{"Pepaya             ", 100, 81},
-	{"Pir                ", 200, 80},
-	{"Pir Hijau          ", 200, 105},
-	{"Pisang Rebus      ", 250, 136},
-	{"Salak              ", 150, 64},
-	{"Semangka           ", 150, 48},
-	{"Alpukat            ", 100, 85},
-	{"Anggur 		     ", 125, 60},
-	{"Klengkeng          ", 200, 79},
-	{"Melon              ", 200, 46},
-	{"Mangga Harum Manis ", 300, 90},
-	{"Pisang Ambon       ", 100, 74},
-	{"Pisang Mas         ", 125, 11},
-	{"Pisang Raja        ", 150, 126},
-	{"Sirsak             ", 125, 55},
+	{"Apel                 ", 160, 92},      //56 
+	{"Apel Merah           ", 140, 82},
+	{"Apel Merah           ", 140, 82},
+	{"Belimbing            ", 160, 80},
+	{"Duku                 ", 200, 81},
+	{"Jambu Air            ", 60, 35},
+	{"Jambu Biji           ", 320, 157},
+	{"Jeruk Medan          ", 140, 46},
+	{"Jeruk Pontianak      ", 150, 67},
+	{"Jeruk Sunkist        ", 200, 40},
+	{"Mangga Manalagi      ", 100, 72},
+	{"Nanas                ", 200, 81},
+	{"Pepaya               ", 100, 81},
+	{"Pir                  ", 200, 80},
+	{"Pir Hijau            ", 200, 105},
+	{"Pisang Rebus         ", 250, 136},
+	{"Salak                ", 150, 64},
+	{"Semangka             ", 150, 48},
+	{"Alpukat              ", 100, 85},
+	{"Anggur 		       ", 125, 60},
+	{"Klengkeng            ", 200, 79},
+	{"Melon                ", 200, 46},
+	{"Mangga Harum Manis   ", 300, 90},
+	{"Pisang Ambon         ", 100, 74},
+	{"Pisang Mas           ", 125, 11},
+	{"Pisang Raja          ", 150, 126},
+	{"Sirsak               ", 125, 55},     //81
 
 
 };
 
 menampikan_karbohidrat() {
-	printf ("\t\t\t\t %c%c=============================================%c%c\n",219,223,223,219);
-	printf ("\t\t\t\t ==-------         Karbohidrat          --------==\n");
-	printf ("\t\t\t\t =================================================\n\n");
+	printf ("\t\t\t\t %c%c===============================================%c%c\n",219,223,223,219);
+	printf ("\t\t\t\t ==-------          Karbohidrat          --------==\n");
+	printf ("\t\t\t\t ===================================================\n");
 	
 	for (i = 0; i < 9 ; i++ ) {
 		printf ("\t\t\t\t %d  | ", i+1);
@@ -117,7 +125,7 @@ menampikan_karbohidrat() {
 		printf ("%d gram\n", Makanan[i].gram);
 	}
 	
-	for	(i = 9; i < 24 ; i++ ) {
+	for	(i = 9; i < 23 ; i++ ) {
 		printf ("\t\t\t\t %d | ", i+1);
 		printf ("%s | ", Makanan[i].nama_makanan);
 		printf ("%d kalori per ", Makanan[i].kalori);
@@ -127,11 +135,11 @@ menampikan_karbohidrat() {
 }
 
 menampikan_lauk() {
-	printf ("\t\t\t\t %c%c=============================================%c%c\n",219,223,223,219);
-	printf ("\t\t\t\t ==-------          Lauk Pauk           --------==\n");
-	printf ("\t\t\t\t =================================================\n\n");
+	printf ("\t\t\t\t %c%c===============================================%c%c\n",219,223,223,219);
+	printf ("\t\t\t\t ==-------           Lauk Pauk            --------==\n");
+	printf ("\t\t\t\t ===================================================\n");
 	
-	for (i = 24; i < 46 ; i++ ) {
+	for (i = 23; i < 44 ; i++ ) {
 		printf ("\t\t\t\t %d | ", i+1);
 		printf ("%s | ", Makanan[i].nama_makanan);
 		printf ("%d kalori per ", Makanan[i].kalori);
@@ -140,13 +148,13 @@ menampikan_lauk() {
 }
 
 menampikan_sayur() {
-	printf ("\t\t\t\t %c%c=============================================%c%c\n",219,223,223,219);
-	printf ("\t\t\t\t ==-------            Sayur             --------==\n");
-	printf ("\t\t\t\t =================================================\n\n");
+	printf ("\t\t\t\t %c%c===============================================%c%c\n",219,223,223,219);
+	printf ("\t\t\t\t ==-------             Sayur              --------==\n");
+	printf ("\t\t\t\t ===================================================\n");
 
-	for (i = 46; i < 60 ; i++ ) {
+	for (i = 44; i < 56 ; i++ ) {
 		printf ("\t\t\t\t %d | ", i+1);
-		printf ("%s | ", Makanan[i].nama_makanan);
+		printf ("%s| ", Makanan[i].nama_makanan);
 		printf ("%d kalori per ", Makanan[i].kalori);
 		printf ("%d gram\n", Makanan[i].gram);
 	}
@@ -154,11 +162,11 @@ menampikan_sayur() {
 }
 
 menampikan_buah() {
-	printf ("\t\t\t\t %c%c=============================================%c%c\n",219,223,223,219);
-	printf ("\t\t\t\t ==-------             Buah             --------==\n");
-	printf ("\t\t\t\t =================================================\n\n");
+	printf ("\t\t\t\t %c%c===============================================%c%c\n",219,223,223,219);
+	printf ("\t\t\t\t ==-------              Buah             --------==\n");
+	printf ("\t\t\t\t ===================================================\n");
 	 
-	for (i = 60; i < 88 ; i++ ) {
+	for (i = 56; i < 83 ; i++ ) {
 		printf ("\t\t\t\t %d | ", i+1);
 		printf ("%s | ", Makanan[i].nama_makanan);
 		printf ("%d kalori per ", Makanan[i].kalori);
@@ -219,11 +227,12 @@ void menghitung_kalori() {
     	printf ("\t\t\t\t%c%c=============================================================%c%c \n",219,220,220,219);
 	
 	daftar_kalori_makanan();
-	printf ("\t\t\t\t %c%c=============================================%c%c\n",219,220,220,219);
+	printf ("\t\t\t\t %c%c===============================================%c%c\n",219,220,220,219);
 	printf("\t\t\t\t\t    Masukkan pilihan makanan : ");
 	scanf("%d", &makanan);
 	fflush(stdin);
 	makanan = makanan - 1;
+	
 	
 	printf("\t\t\t\t\t    Masukkan jumlah makanan (Gram) : ");
 	scanf("%d", &jumlah_gram);
@@ -234,11 +243,28 @@ void menghitung_kalori() {
 	total_kalori += kalori_makanan;
 	
 	printf("\n\t\t\t\t\t    jumlah kalorinya yaitu : %d kalori\n",  kalori_makanan);
+	
+	strncpy (yangDimakan[j].nama_makanan,Makanan[makanan].nama_makanan,21);
+	yangDimakan[j].kalori = kalori_makanan;
+	
+	j++;
+		
+	printf("\n\t\t\t\t\t    Makanan anda\n",  kalori_makanan);
+	for (i = 2; i < j; i++){
+		printf("\t\t\t\t\t    %s |: %d  kalori  \n", yangDimakan[i].nama_makanan ,yangDimakan[i].kalori);
+	
+	}
+	
 	printf("\t\t\t\t\t    total kalori : %d kalori\n", total_kalori);
-	printf("\n\t\t\t\t\t    ingin menambah kalori pada makanan lain? (1/0)\n");
+
+	
+	printf("\n\t\t\t\t\t    ingin menambah kalori pada makanan lain? (1/0) :");
 	scanf ("%d", &menambah);
 	fflush(stdin);
 	
+
+	
+		
 	switch (menambah) {
 		case 0 : {
 		system ("cls");
